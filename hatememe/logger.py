63dab1ba3ddc520ff24f7a3818d2a675b10cf4a8
Ridwan Salahuddeen <ridzy619@yahoo.com>
2022-12-04 02:46:16 +0400

    Sync run config with WandB

--- hatememe/logger.py
+++ hatememe/logger.py
@@ -6,8 +6,8 @@ now = datetime.now()
 
 cfg = CFG()
 
-wandb.init(project="hatememe", entity="team-g11", name=f"Experiment_{now}")
+config = {key:val for key, val in cfg.__class__.__dict__.items() if isinstance(val, (float, int, str, bool))}
 
-wandb.config = CFG.__class__.__dict__
+wandb.init(project="hatememe", entity="team-g11", name=f"Exp_{now}", config=config)
 
 log = wandb.log
