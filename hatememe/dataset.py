f0d4c1605d6b6fcc2712bfb28ff96f6b733f17dc
Ridwan Salahuddeen <ridzy619@yahoo.com>
2022-12-04 02:45:35 +0400

    Add memotion dataset

--- hatememe/dataset.py
+++ hatememe/dataset.py
@@ -7,9 +7,9 @@ from PIL import Image
 
 from hatememe.config import CFG
 
-HOME = os.environ.get("HOME")
-images_path = f"{HOME}/.cache/torch/mmf/data/datasets/hateful_memes/defaults/images/img/"
-annotations_path = f"{HOME}/.cache/torch/mmf/data/datasets/hateful_memes/defaults/annotations/"
+# HOME = os.environ.get("HOME")
+# images_path = f"{HOME}/.cache/torch/mmf/data/datasets/hateful_memes/defaults/images/img/"
+# annotations_path = f"{HOME}/.cache/torch/mmf/data/datasets/hateful_memes/defaults/annotations/"
 
 class HMDataset(Dataset):
     def __init__(
@@ -19,6 +19,7 @@ class HMDataset(Dataset):
         image_transform=None,
         text_transform=None,
         eager_transform=False,
+        add_memotion=False
 
     ) -> None:
 
@@ -27,6 +28,11 @@ class HMDataset(Dataset):
         ), f"Invalid annotation file format. Format should be '.jsonl', not {annotation_path.split('.')[0]}"
 
         self.annotation: pd.DataFrame = pd.read_json(annotation_path, lines=True)
+
+        if add_memotion:
+            memotion_annotation: pd.DataFrame = \
+                pd.read_json(f"{annotation_path}/label_memotion.jsonl", lines=True)
+            self.annotation = pd.concat([self.annotation, memotion_annotation], axis=0)
 
         self.images_paths = [os.path.join(images_path, self.annotation.loc[index, "img"]) for index in range(self.__len__())]
 
@@ -58,64 +64,3 @@ class HMDataset(Dataset):
         
         return self.transformed_images[index], self.transformed_texts[index], self.transformed_labels[index]
 
-
-
-def create_dataloader(
-        images_path: str = CFG.images_path,
-        annotation_path: str = f"{CFG.annotations_path}/train_updated.jsonl",
-        batch_size: int = CFG.batch_size,
-        image_transform = None,
-        text_transform = None,
-        shuffle = True,
-    ):
-    """Function to create dataloader given image path and absolute path for one of
-    train.jsonl, dev_unseen.jsonl and test_unseen.jsonl
-
-    images_path (str): path to the folder containing all the images
-    annotation_path (str): path to a jsonl file containing the data corresponding 
-        to train, test or dev
-    batch_size (int): batch size for loading the data
-    image_transform (function): A function to use to transform the image data
-    text_transform (function): A function to use to transform the text data
-    shuffle (bool): To shuffle the data or not
-
-    
-    """
-
-    hm_dataset = HMDataset(
-        images_path, 
-        annotation_path, 
-        image_transform=image_transform, 
-        text_transform=text_transform
-    )
-
-    hm_dataloader = DataLoader(
-        hm_dataset,
-        batch_size=batch_size,
-        shuffle=shuffle,
-    )
-
-    return hm_dataloader
-
-if __name__=="__main__":
-    CFG = CFG()
-    # # print(CFG.text_transform("Hello, world!"))
-    # train_dataset = HMDataset(
-    #     images_path,
-    #     os.path.join(annotations_path,'train_updated.jsonl'),
-    #     image_transform=CFG.image_transform,
-    #     text_transform=CFG.text_transform,
-    #     eager_transform=True
-    # )
-    # train_dataloader = DataLoader(train_dataset, 4, shuffle=True)
-    # import time
-    # start = time.time()
-    # for n, data in enumerate(train_dataloader):
-    #     print(n)
-    #     i, t, l = data
-    #     end = time.time()
-    #     print("Duration:", end - start)
-    #     start = end
-
-    #     if n > 5:
-    #         break
