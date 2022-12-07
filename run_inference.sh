c8fa852ef422127a16b9f140a5bf52a4d96ac5d0
Ridwan Salahuddeen <ridzy619@yahoo.com>
2022-12-07 19:10:33 +0400

    Run inference script

---
+++ run_inference.sh
@@ -0,0 +1,6 @@
+ssh 10.127.30.13 /home/ridwan.salahuddeen/.conda/envs/clipenv/bin/python \
+    /home/ridwan.salahuddeen/Documents/ML701/Project/Hatememe_project/inference.py \
+    --mode=inference \
+    --experiment_name="Exp_2022-12-05\ 00:26:05.400515" \
+    --data_split=test-dev-all \
+    --run_id=gr0xgh7b
